c52b425142449097c0d3ea0e79aebb3ce94162aa
Wren <wren_xing@163.com>
2023-01-15 17:26:16 +0800

    remove sensitive info
    才发现我的API账号不小心给我丢到这了，蠢哭自己

--- config_example.py
+++ config_example.py
@@ -1,3 +1,3 @@
-APPID = '20230107001523093'  # 请注册你自己的密钥
-APPKEY = 'LiinO0W7NmmF6RHldPWX'  # 请注册你自己的密钥
+APPID = '2***************3'  # 请注册你自己的密钥
+APPKEY = 'L***************X'  # 请注册你自己的密钥
 WORK_PATH ='./config' # 要翻译的文件目录，此目录下所有snbt文件都会被翻译【可选./ftbquests或./chapter，./chapter只翻译章节内容，./ftbquests额外包括战利品表名称、大章节标题等内容】
