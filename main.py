2d6b68b97a554ba6e4e8db03576b9bd9d1c094f5
XDawned <12695467853@qq.com>
2025-05-09 00:21:49 +0800

    feat:补全使用说明

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ if __name__ == '__main__':
         import func
         while True:
             print('**********FTBQ本地化小工具-%s**********' %VERSION)
-            print('主功能引导\n1.翻译任务\n2.生成lang文件(默认保存位置config中WORK_PATH)\n3.翻译lang文件(低版本json,高版本snbt)\n4.回填lang文件\n5.退出')
+            print('主功能引导\n'
+                  '1.翻译任务\n  (1.20+自带lang文件，请修改config中LANG_PATH后选择功能3, lang文件见/ftbquests/quests/lang/en_us.snbt)\n'
+                  '2.生成lang文件\n  (默认保存位置config中WORK_PATH)\n'
+                  '3.翻译lang文件\n  (低版本json,高版本snbt)\n'
+                  '4.回填lang文件\n  (不支持回填高版本snbt)\n'
+                  '5.退出'
+                  )
             choice = input('请输入你要选择的功能：')
             if choice == '1':
                 func.quest_trans()
